fca8222893fb92f4ef0d0f4ce00e957519f6745b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-11 20:42:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # TODAY WE WILL LEARN BASICS OF PYTHON IN THIS REPO
-
+#Python is implicit typed language ----> Simply means we don't have to tell the type of the variable
+#BASIC PRINTING
 print("Welcome Bhai , Karein start?")
 print("Hello World")
 
@@ -7,11 +8,24 @@ print("My name is","Girdhari lal")
 print(19)
 print(19-19)
 
-
-name = "Girdhari lal"
+#VARIABLES
+name = "Girdhari lal"       #Also equals to '''Girdhari lal''' & 'Girdhari lal'
 age = 19
 price = 25.99
 
 print("name")
-print("My name is :", name) 
-print("I am",age,"year old")
+print("My name is :", name)         # My name is : Girdhari lal
+print("I am",age,"year old")        # I am 19 year old
+
+print(type(age))                    # tells the type of variable
+
+#DATATYPES
+#Integers-Strings-Float-Boolean-None
+
+#BOOLEAM    ====> True/False
+
+Old = False
+a = None
+
+print(type(Old))
+print(type(a))              #None Type
