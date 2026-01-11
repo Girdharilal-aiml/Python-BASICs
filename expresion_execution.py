8f1b64756627482dca486ca4c16eb32f963e68da
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-11 20:52:44 +0500

    Update expresion_execution.py

--- expresion_execution.py
+++ expresion_execution.py
@@ -1,12 +1,13 @@
 A,B=2,4
 txt="@"
+C="3"
 
 #String and Numeric values can perform together using "*"
 
 print(2*txt*4)          # Result is 8 times @  -----> Inshort Multiplication by number Multiplies String
 
 #String and String can operater with + 
-C="3"
-print((C+txt)*B)        # Result is 3@3@3@3@   -----> Inshort use + to join two string variables
+
+print((C+txt)*B)        #(CONCATENATION) Result is 3@3@3@3@   -----> Inshort use + to join two string variables
 
 
