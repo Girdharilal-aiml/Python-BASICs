6458b71424e199e847d365800f91a6b7a2f1ba73
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-11 15:08:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome Bhai , Karein start?")
+print("Hello World")
+
+print("My name is","Girdhari lal")
+print(19)
+print(19-19)
+
+
+name = "Girdhari lal"
+age = 19
+price = 25.99
+
+print("name")
+print("My name is :", name) 
+print("I am",age,"year old")
