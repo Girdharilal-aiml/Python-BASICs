076824b800cb507f686c0febf69bc39902c602b1
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-14 17:19:30 +0500

    Create type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,12 @@
+a = 2
+b = 4.5
+
+print(a+b)
+
+c = "0.5"       #now it is a string 
+
+# print(a+c)       this will give error
+
+c = float(c)     #This is type conversion from string to float
+print(a+c)
+
