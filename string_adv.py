b097ed5235eafae6cfa4d8e9b17802830802c731
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-16 13:49:58 +0500

    Update string_adv.py

--- string_adv.py
+++ string_adv.py
@@ -1,2 +1,22 @@
 # STRING FUNCTIONS
 
+str1 = "i am coder."
+
+str1.endswith("ER")                         #returns true is string ends with substr
+print(str1.endswith("er.")) 
+
+
+str1.capitalize()                           #capitablizes 1st char
+print(str1.capitalize())
+
+
+# str.replace(old,new)                      replaces all occurrences of old with new
+str1.replace("coder","designer")
+str1 = str1.replace("coder","designer")
+print(str1)
+
+
+# str.find(word)                            returns 1st index of 1st occurrence
+print(str1.find("a"))
+
+str1.count("AM")                            #counts the occurrence of substr in string
