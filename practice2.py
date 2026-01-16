b68ffd0d027479f9815f6524f9b1083187949d49
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-16 14:01:28 +0500

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,3 +1,12 @@
+#
+
+
+#WAP TO FIND OCCURENCE OF "$" IN A STRING
+
+str1 = "Hi, I$am name$ , how we $ find $."
+print(str1.count("$"))
+
+
 #WAP TO TAKE USER NAME AND FIND LENGTH
 
 name = input("Your name: ")
