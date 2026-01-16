678b1ae93cc9590bf2f807402723ec66e308fb8d
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-16 12:54:37 +0500

    Create Strings1.py

---
+++ Strings1.py
@@ -0,0 +1,16 @@
+#WAYS OF WRITING STRINGS
+str1 = "This is a string."
+str2 = 'This is also a string.'
+str3 = """And this is also a string."""
+
+#BASIC OPERATOR
+str4 = "String with line break. Here comes next line"               
+str5 = "String with line break. \nHere comes \tnext line"         # \n for new line and \t for tab space
+
+print(str4)
+print(str5)
+
+str6 = "HELLO"
+str7 = "WORLD"
+print(str6+str7)        # + for concatenating
+
