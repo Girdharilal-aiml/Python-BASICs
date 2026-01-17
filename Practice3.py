1888456b626820ec3893eac76d122dc4080fb02b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-17 13:48:00 +0500

    Create Practice3.py

---
+++ Practice3.py
@@ -0,0 +1,19 @@
+# WAP to check if a list contains a palindrome of elements. (Hint: use copy() method )
+
+list2 = [1,2,3,2,1]
+
+
+
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+
+mov1 = input("Enter first movie name: ")
+mov2 = input("Enter second movie name: ")
+mov3 = input("Enter third movie name: ")
+
+list1 = []
+
+list1.append(mov1)
+list1.append(mov2)
+list1.append(mov3)
+
+print(list1)
