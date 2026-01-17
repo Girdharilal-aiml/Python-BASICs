46ca2da4322baddd4148b15605bd3c7e714f6116
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-17 14:05:16 +0500

    Update Practice3.py

--- Practice3.py
+++ Practice3.py
@@ -1,7 +1,24 @@
+# WAP to count the number of students with the "A" grade in the following tuple
+
+grade = ("C","D","A","A","B","B","A")
+print(tuple1.count("A"))
+
+
 # WAP to check if a list contains a palindrome of elements. (Hint: use copy() method )
 
 list2 = [1,2,3,2,1]
+print(list2)
 
+list3 = list2.copy()
+print(list3)
+
+list3.reverse()
+print(list3)
+
+if(list2 == list3):
+    print("PALINDROME")
+else: 
+    print("NOT PALINDROME")
 
 
 # WAP to ask the user to enter names of their 3 favorite movies & store them in a list
