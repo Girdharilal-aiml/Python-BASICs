d0739bd7f2e23ef863ecc196b9a5c649487b73cc
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-17 14:08:50 +0500

    Update Practice3.py

--- Practice3.py
+++ Practice3.py
@@ -1,7 +1,14 @@
+# WAP to store the above values in a list & sort them from "A" to "D"
+
+grade = ["C","D","A","A","B","B","A"]
+grade.sort()
+print(grade)
+
+
 # WAP to count the number of students with the "A" grade in the following tuple
 
 grade = ("C","D","A","A","B","B","A")
-print(tuple1.count("A"))
+print(grade.count("A"))
 
 
 # WAP to check if a list contains a palindrome of elements. (Hint: use copy() method )
