a56a4d1d94fd3e3d7061a6c6f65afe51bb61daaf
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-17 12:31:10 +0500

    Create Lists1.py

---
+++ Lists1.py
@@ -0,0 +1,15 @@
+""" A Built-in data types that stores set of values
+    It can store elements of DIFFERENT TYPES (integer, float, string, etc) """
+
+# List is Mutable
+
+# variable = [value1,value2,...]            ---> LIST
+
+marks = [21.2, 2143.4, 45.5, 65.7]
+print(marks)
+print(type(marks))
+print(len(marks))
+
+
+Student1 = ["Girdhari lal", 19 , 98.5]
+print(Student1)
