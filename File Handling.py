e4fecab7c32d495e6b472baa80d4396f011c2ab2
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 17:00:01 +0500

    Update File Handling.py

--- File Handling.py
+++ File Handling.py
@@ -1,3 +1,7 @@
 # FILE HANDLING MEANS CRUD OPERATIONS ON FILES
 # CREATING, READING, UPDATING, DELETING
 
+r = open("Superman.txt", "w")       # w means write mode, it will create a new file if it doesn't exist
+r.write("Superman is the best superhero in the world.")
+r.close()       # closing the file after writing
+
