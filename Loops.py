557eb459ae1eec324b33cd67b75db24e186eb703
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 17:01:37 +0500

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -3,5 +3,12 @@
 # While works on Condition
 # For works on Number
 
+# FOR LOOP                  ---> range(Start, Stop, Steps)
+a = range(1,20,1)
+for i in a:
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
 
 
