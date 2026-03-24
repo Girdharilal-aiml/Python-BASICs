ac3d847d99d997f7b50eb3abed7624b012186398
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 16:57:48 +0500

    Create Data Structures.py

---
+++ Data Structures.py
@@ -0,0 +1,7 @@
+# Data structures are used to store, organize, and manipulate 
+# data efficiently. Python provides several built-in data 
+# structures
+# And for storing multiple values we will again use variables
+
+# Now in python we have 4 types of in-build data structure 
+# List, Tuple, Dictionary, Set 
