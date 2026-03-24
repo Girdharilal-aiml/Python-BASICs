98515b4173421fecca4660c52581ac880240d468
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 17:00:06 +0500

    Update File Handling.py

--- File Handling.py
+++ File Handling.py
@@ -5,3 +5,6 @@ r = open("Superman.txt", "w")       # w means write mode, it will create a new f
 r.write("Superman is the best superhero in the world.")
 r.close()       # closing the file after writing
 
+r = open("Superman.txt", "a")       # a means append mode, it will add new content to the existing file without overwriting it
+r.write("adding new content with a = append")
+r.close()       
