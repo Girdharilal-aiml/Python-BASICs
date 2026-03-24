5a479f126618f669c54c752c4fcc6520dc4d8cf3
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 16:59:29 +0500

    Update Exception_Handling.py

--- Exception_Handling.py
+++ Exception_Handling.py
@@ -8,3 +8,15 @@ except ValueError as e:
     print("Error:", e)
 
 
+
+a = int(input("Enter a number: "))
+try:
+    result = 10 / a
+    print("Result:", result)    
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+else:
+    print("Division successful.")
+
+finally:    print("Execution completed.")
