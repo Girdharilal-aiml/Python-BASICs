a3618e4774ede341ef44610cc2ffa9c92af45da3
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-25 15:53:51 +0500

    Update P1.py

--- PRACTICE PROBLEMS FOR BEGINER/P1.py
+++ PRACTICE PROBLEMS FOR BEGINER/P1.py
@@ -5,4 +5,9 @@ from pathlib import Path
 import os
 
 
+print("1 for Creating a file")
+print("2 for Reading a file")
+print("3 for Appending a file")
+print("4 for Deleting a file")
+choice = int(input("Enter your choice: "))
 
