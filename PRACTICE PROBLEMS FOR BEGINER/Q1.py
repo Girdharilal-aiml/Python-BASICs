8e67101c652500c40fcf73f7d2745f5cf2d8ea2e
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-25 15:51:43 +0500

    Update Q1.py

---
+++ PRACTICE PROBLEMS FOR BEGINER/Q1.py
@@ -0,0 +1,11 @@
+# CHECK LIST IS SORTED OR NOT
+# a = [1,9,3,2,5,6,4,8,7]
+# ind = 0
+# for i in range(1, len(a)):
+#     if a[i] < a[ind]:
+#         print("Not Sorted")
+#         break
+#     ind = i
+
+
+
